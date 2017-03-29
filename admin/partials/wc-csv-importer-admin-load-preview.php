b0af0293f737046b9c81f1dbb274a218a065a9e5
poanchen <chen.baron@hotmail.com>
2017-03-29 05:39:36 +0000

    whenever swap the value in the dropdown, the same identical one will swap as well, all the dropdown and default are just hard coded for now, but in the future those value will be getting from the DB and it will work with the current code that we have

--- admin/partials/wc-csv-importer-admin-load-preview.php
+++ admin/partials/wc-csv-importer-admin-load-preview.php
@@ -13,6 +13,7 @@
  */
 
 $c = new col();
+$_SESSION["numberOfCols"] = 0;
 $listOfProducts = array();
 $fileData = fopen( $_SESSION["file"], 'r' );
 
@@ -23,9 +24,13 @@ for ( $i = 0; !feof( $fileData ); $i++ ) {
 		continue;
 	}
 
+	if ( count($eachProduct) > $_SESSION["numberOfCols"] ) {
+		$_SESSION["numberOfCols"] = count($eachProduct);
+	}
+
 	$new_product_post = array();
 
-	for ( $j = 0; $j < count($c->cols_default); $j++ ) {
+	for ( $j = 0; $j < count($eachProduct); $j++ ) {
 		$new_product_post[$c->cols_default[$j]['wc_field_name']] = $eachProduct[$j];
 	}
 
@@ -51,10 +56,11 @@ $_SESSION["listOfProducts"] = $listOfProducts;
 	<table>
 		<tr>
 			<?php
-				for ( $i = 0; $i < count( $c->cols_default ); $i++ ) {
+				for ( $i = 0; $i < $_SESSION["numberOfCols"]; $i++ ) {
 			?>
 					<th>
 						<select id="<?php echo 'wc_field_' . $i; ?>">
+							<option value="" selected></option>
 			<?php
 							for ( $j = 0; $j < count( $c->cols_default ); $j++ ) {
 								$value = $c->cols_default[$j]['name'];
@@ -76,7 +82,7 @@ $_SESSION["listOfProducts"] = $listOfProducts;
 		?>
 				<tr>
 				<?php
-					for ( $j = 0; $j < count( $c->cols_default ); $j++ ) {
+					for ( $j = 0; $j < $_SESSION["numberOfCols"]; $j++ ) {
 						$wc_field_name = $c->cols_default[$j]['wc_field_name'];
 				?>
 						<th><?php echo $listOfProducts[$i][$wc_field_name]; ?></th>
@@ -94,21 +100,63 @@ $_SESSION["listOfProducts"] = $listOfProducts;
 	// thanks to https://www.w3schools.com/js/js_cookies.asp
 	function setCookie(cname, cvalue, exdays) {
 		var d = new Date();
+
 		d.setTime( d.getTime() + ( exdays*24*60*60*1000 ) );
+
 		var expires = "expires="+ d.toUTCString();
+
 		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	}
+
+	// thanks to https://www.w3schools.com/js/js_cookies.asp
+	function getCookie(cname) {
+		var name = cname + "=";
+		var decodedCookie = decodeURIComponent( document.cookie );
+		var ca = decodedCookie.split( ';' );
+
+		for( var i = 0; i < ca.length; i++ ) {
+			var c = ca[i];
+
+			while ( c.charAt( 0 ) == ' ' ) {
+				c = c.substring( 1 );
+			}
+			if ( c.indexOf( name ) == 0 ) {
+				return c.substring( name.length, c.length );
+			}
+		}
+
+		return "";
+	}
+
+	function swapTheOptionsIfNeeded(currentOptionId, currentOptionName) {
+		for ( var i = 0; i < <?php echo $_SESSION["numberOfCols"]; ?>; i++ ) {
+			if ( i == currentOptionId ) {
+				continue;
+			}
+			if ( currentOptionName == getOptionNameByIndex( document.getElementById( "wc_field_" + i ).selectedIndex) ) {
+				document.getElementById( "wc_field_" + i ).value = getCookie( 'previousSelection' );
+			}
+		}
+	}
+
+	function getId(fieldId) {
+		return parseInt( fieldId.replace( /[^\d.]/g, '' ) );
 	}
 
 	function wcFieldHasBeenClick(event) {
 		var e = event.target;
+
+		swapTheOptionsIfNeeded( getId( e.id ), getOptionNameByIndex( e.selectedIndex ) );
+
 		setCookie( e.id, getOptionNameByIndex( e.selectedIndex ) );
+		setCookie( 'previousSelection', getOptionNameByIndex( e.selectedIndex ) );
 	}
 
 	function getOptionNameByIndex(index) {
 		return document.getElementsByTagName("option")[index].value;
 	}
 
-	for ( var i = 0; i < <?php echo count( $c->cols_default ); ?>; i++ ) {
+	for ( var i = 0; i < <?php echo $_SESSION["numberOfCols"]; ?>; i++ ) {
 		setCookie( "wc_field_" + i, getOptionNameByIndex( document.getElementById( "wc_field_" + i ).selectedIndex) );
 		document.getElementById( "wc_field_" + i ).addEventListener( "click", wcFieldHasBeenClick );
 	}
